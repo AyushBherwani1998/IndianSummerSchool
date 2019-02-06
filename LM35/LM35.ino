e2ccf6e9d0a599b2fe650a9168ed78467777f893
AyushBherwani1998 <34301187+AyushBherwani1998@users.noreply.github.com>
2019-02-06 12:06:05 +0530

    Update LM35.ino

--- LM35/LM35.ino
+++ LM35/LM35.ino
@@ -1,14 +1,20 @@
 const int LM35 = A0;
-float adcValue;
+float temp;
 void setup() {
   // put your setup code here, to run once:
   pinMode(LM35,INPUT);
+  pinMode(13,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  adcValue = analogRead(LM35);
-  Serial.println(adcValue/2);
+  temp = analogRead(LM35)/2;
+  if(temp>30){
+    digitalWrite(13,1);
+    }else{
+      digitalWrite(13,0);
+      }
+  Serial.println(adcValue);
   delay(1000);
 }
